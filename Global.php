cf7cd64cd0c7cd1663cdca945599d3c011bab383
Montarones <nhed@laposte.net>
2016-05-10 13:27:16 -0400

    modif gloabal

--- Global.php
+++ Global.php
@@ -38,40 +38,7 @@ $manager = new moustache_userManager($db);
 
 
 
-// CREER CE COMPTE
-if (isset($_POST['jakie']))
-{
-	if(!$manager->exists($_POST['pseudo']))
-	$perso = new moustache_user(['pseudo' => $_POST['pseudo'], 'password' => $_POST['pass']]);
-	$manager->add($perso);
-	
-	if(!$manager->exists($_POST['pseudo2']))
-	$perso2 = new moustache_user(['pseudo' => $_POST['pseudo2'], 'password' => $_POST['pass2']]);
-	$manager->add($perso2);
-}
 
-
-// UTILISER CE COMPTE
-if (isset($_POST['utiliser']))
-{
-	$user=$manager->utiliser($_POST['pseudo'], $_POST['pass']);
-	$user2=$manager->utiliser($_POST['pseudo2'], $_POST['pass2']);
-	
-	if (!$manager->estAmi($user->id(), $user2->id())) 
-	{
-	$manager-> ajoutAmi($user, $user2);
-	}
-	else
-	{
-		echo 'Vous êtes déja amis !';
-	}
-	if ($manager->estAdmin($user->pseudo()))
-	{
-	echo 'Vous êtes admin !';
-	}
-	
-	
-}
 
 
 
