762e23b5e8a1efe7805b1ba8e85b933b7638b9e1
ads <ads@neuroshok.com>
2016-05-10 19:47:33 +0200

    add user class

---
+++ include/user.php
@@ -0,0 +1,17 @@
+<?php
+
+class user
+{
+	
+	__construct(id);
+	
+	
+	
+	static function add(name, email, pass);
+	static function edit(field_name, value);
+	static function exist(user_name);
+	static function is_friend(user_id_1, user_id_2);
+
+}
+
+?>
